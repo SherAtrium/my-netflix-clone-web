324ed62cfd3ce0ea873e31996d13e38419f32eb7
Sherzod Mamatmurodov <sherzodm214@gmail.com>
2021-09-13 00:35:54 +0500

    Tasks done!

---
+++ src/Components/FunctionalComponent.js
@@ -0,0 +1,3 @@
+export const FunctionalComponent = () => (
+  <h4>React Functional Component</h4>
+)
---
+++ src/Components/ReactPureComponent.js
@@ -0,0 +1,13 @@
+import React, { PureComponent } from 'react';
+
+class ReactPureComponent extends PureComponent {
+  render() {
+    return (
+      <h3>
+        React Pure Component
+      </h3>
+    )
+  }
+};
+
+export default ReactPureComponent;
