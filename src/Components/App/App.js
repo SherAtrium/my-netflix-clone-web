3a8ee8fa8addfc26921f08f755d79423bc57f60d
Sherzod Mamatmurodov <sherzodm214@gmail.com>
2021-09-21 13:18:01 +0500

    test commit

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,7 +1,14 @@
+import React from 'react';
 import Styles from './App.module.scss';
 
-const App = () => {
-  return <div className={Styles.container}>Hello React!</div>;
-};
+console.log('one');
+var ww = process.env.NODE_ENV.trim.toLowerCase();
+if (ww === 'production') {
+  console.log('Another one');
+} else {
+  console.log('test');
+}
+
+const App = () => <div className={Styles.container}>Hello React!</div>;
 
 export default App;
